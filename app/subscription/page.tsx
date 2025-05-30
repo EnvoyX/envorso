72b07e02faa91ae64dfde608c67b097fe7b0fe43
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-05-30 23:46:55 +0700

    feat : added Clerk Subscription Checks as optional feature, and fix subscription layout page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -2,7 +2,8 @@ import { PricingTable } from "@clerk/nextjs";
 
 const Subcription = () => {
   return (
-    <div>
+    <div className="flex flex-col m-6 items-center justify-center">
+      <h1 className="text-3xl font-bold mb-10">Subscription Plans</h1>
       <PricingTable />
     </div>
   );
